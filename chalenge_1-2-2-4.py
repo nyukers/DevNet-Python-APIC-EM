b964fdc6c88e8498e3fc38170041465bb01f357b
Zibnitskii Aleksandr <a.zibnitskiy@gmail.com>
2020-01-29 23:21:52 +0200

    Update chalenge_1-2-2-4.py

---
+++ chalenge_1-2-2-4.py
@@ -0,0 +1,12 @@
+#
+# Author: Zibnitskii Aleksandr
+# Condition: Try writing a print() statement with a space between the
+# words without using a variable to create the space.
+#
+space = " "
+str1 = "Cisco"
+str2 = "Networking"
+str3 = "Academy"
+resultStr = space.join([str1, str2, str3])
+print(resultStr)
+
