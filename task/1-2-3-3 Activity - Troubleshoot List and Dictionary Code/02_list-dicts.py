c0d922617bce83254e7140caa0341fd4f355f315
Aleksandr Zibnitskii <a.zibnitskiy@gmail.com>
2020-01-30 15:14:55 +0200

    Update files

--- task/1-2-3-3 Activity - Troubleshoot List and Dictionary Code/02_list-dicts.py
+++ task/1-2-3-3 Activity - Troubleshoot List and Dictionary Code/02_list-dicts.py
@@ -7,11 +7,6 @@ country = [
             "South Africa"
           ]
 
-"""Create a dictionary of BRICS capitals.
-Note that South Africa has 3 capitals. Therefore, we embed a list inside
-the dictionary.
-"""
-
 capitals = {
     "South Africa": [
                         "Pretoria",
@@ -27,14 +22,4 @@ capitals = {
 # Print the list and dictionary
 print( country )
 print( capitals )
-"""
-What response did you get?
-Why did the list and dictionary contents not print?
-Fix the code and run the script again.
-"""
-
-print( capitals["South Africa"[1]] )
-"""
-Why did you get an error for the 2nd capital of South Africa?
-Hint: Check the syntax for the index value.
-"""
+print( capitals["South Africa"][1] )
